8cb993af61d7ed5e42c5c834f086dc825fd853cf
Monika Majer <majer_monika@interia.pl>
2018-12-26 22:10:51 +0100

    Add js code, add pug files

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'pug');
+
+app.set('views','./views');
+
+app.get( '/login', function( req, res ) {
+    res.render( 'login', {
+        url: "/auth/google",
+        
+    });
+});
+
+app.get( '/auth/google', function( req, res) {
+    res.render( 'google', {
+        user: {
+            first_name: 'Monika',
+            last_name: 'Majer'
+        }
+    });
+});
+
+app.listen(3000);
+app.use(function (req, res, next) {
+    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
+});
